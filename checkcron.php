3f1e62004214a0f5fb5ff974533b32cdb37bf1da
Alexander Nieto <fortunes.technology@gmail.com>
2022-11-18 04:24:56 +0900

    send email after checking cron

---
+++ checkcron.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'dbconfigcron.php';
+include_once 'PHPMailer\send_email.php';
+
+$count = $crud->checkCronSatus('Running');
+if($count > 0) {
+    send_email("samsamuel20101@gmail.com");
+    exit();
+}
+?>
