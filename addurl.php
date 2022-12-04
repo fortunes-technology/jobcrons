18f70ab750158737418288a251b398ca39b32129
Alexander Nieto <fortunes.technology@gmail.com>
2022-12-04 19:58:17 +0900

    upadate web responsive

--- addurl.php
+++ addurl.php
@@ -104,14 +104,14 @@
                                                   <i class="fas fa-check ml-1"></i></button>
                                             </div>
                                             <div class="col-lg-6 px-sm-1 col-sm-12 mt-2 mb-2 check-label">
-                                                <label style="width: 350px; margin-left: 25px;" class="form-check-label" for="willCountryCheck">Will add addressCountry tag?</label>
+                                                <label style="width: 350px; margin-left: 0px;" class="form-check-label" for="willCountryCheck">Will add addressCountry tag?</label>
                                                 <input style="height: 30px;" type="checkbox" class="form-control form-control-lg"  id="willCountryCheck" name="willCountryCheck"> 
                                             </div>
                                             <div class="col-lg-4 px-sm-1 col-sm-12 mt-2 mb-2">
                                                 <input type="text" class="form-control form-control-lg" id="willAddCountry" name="willAddCountry" placeholder="Add Country Value">
                                             </div>
                                             <div class="col-lg-6 px-sm-1 col-sm-12 mt-2 mb-2 check-label">
-                                                <label style="width: 350px; margin-left: 25px;" class="form-check-label" for="willLocationCheck">Will add jobLocationType tag?</label>
+                                                <label style="width: 350px; margin-left: 0px;" class="form-check-label" for="willLocationCheck">Will add jobLocationType tag?</label>
                                                 <input style="height: 30px;" type="checkbox" class="form-control form-control-lg"  id="willLocationCheck" name="willLocationCheck"> 
                                             </div>
                                             <div class="col-lg-4 px-sm-1 col-sm-12 mt-2 mb-2">
